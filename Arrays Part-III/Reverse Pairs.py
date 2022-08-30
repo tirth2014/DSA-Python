2d3eed9e5761bd7d742a3b004136e4fd9c1889fb
Tirth Patel <tirth99092@gmail.com>
2022-08-30 07:18:01 +0530

    Create Reverse Pairs.py

---
+++ Arrays Part-III/Reverse Pairs.py
@@ -0,0 +1,48 @@
+# Using Enhanced MergeSort
+
+class Solution:
+    def reversePairs(self, arr):
+        count = 0
+        if len(arr) > 1:
+            mid = len(arr) // 2
+            L = arr[:mid]
+            R = arr[mid:]
+            
+            count += self.reversePairs(L)
+            count += self.reversePairs(R)
+            
+            # Ye hai Main Logic - O(N) kyuki j ko hum jaha hai wahi se aage badha rahe...na ki har baar starting se start kar rhe 
+            j = 0
+            for i in range(len(L)):
+                while j < len(R) and L[i] > 2*R[j]:
+                    j+=1
+                count += j
+
+            # Merge    
+            i = j = k = 0
+            while i < len(L) and j < len(R):
+                if L[i] < R[j]:
+                    arr[k] = L[i]
+                    i += 1
+                else:
+                    # for r in range(i, len(L)):
+                    #     if L[r] > 2 * R[j]:
+                    #         self.count += 1
+                    arr[k] = R[j]
+                    j += 1
+                k += 1
+
+            while i < len(L):
+                arr[k] = L[i]
+                i += 1
+                k += 1
+            while j < len(R):
+                arr[k] = R[j]
+                j += 1
+                k += 1
+        return count
+      
+# T.C : O(NlogN) + O(N) + O(N)                
+            
+                    
+                
