217be45d113e266286e2f52151fe0341302aa4bc
Tirth Patel <tirth99092@gmail.com>
2022-08-31 08:07:59 +0530

    Update BinaryIndexedTree (Fenwick Tree).py

--- BinaryIndexedTree (Fenwick Tree).py
+++ BinaryIndexedTree (Fenwick Tree).py
@@ -19,7 +19,7 @@ class BIT:
 if __name__ == "__main__":
     arr = list(map(int, input("enter array elements with comma in between: ").strip().split(',')))
     tree = BIT(len(arr))
-    print("Input List with Index: ", {i : arr[i] for i in range(len(arr))})
+    print("Input List with Index: ", {i:n for i,n in enumerate(arr)})
     for i in range(1, len(arr) + 1):
         tree.update(i, arr[i - 1])
     print('The BIT is: ', {i: tree.bit[i] for i in range(len(tree.bit))})
@@ -30,7 +30,7 @@ if __name__ == "__main__":
     i = int(input("Enter the index to Update the value at: "))
     x = int(input("enter the value to be added at index {0}: ".format(i)))
     tree.update(i, x)
-    print('The updated BIT is: ', {i: tree.bit[i] for i in range(len(tree.bit))})
+    print('The updated BIT is: ', {i: n for i,n in enumerate(tree.bit)})
     i = int(input("Enter the index till which you want sum: "))
     res = tree.sum(i)
     print("The sum of elements till {0} after updation is: {1}".format(i, res))
