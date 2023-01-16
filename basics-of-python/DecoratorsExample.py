b9b5565af336e7d5afdf0389cdbd08f9771fa0a2
Tirth Patel <tirth99092@gmail.com>
2023-01-16 12:03:35 +0530

    Update DecoratorsExample.py

--- basics-of-python/DecoratorsExample.py
+++ basics-of-python/DecoratorsExample.py
@@ -1,22 +1,46 @@
 # DECORATORS EXAMPLE:
 # LearnHere: https://book.pythontips.com/en/latest/decorators.html
 
-
 def im_decorator(a_fun):
     def wrapperFun():
         print("I'm flower executed before a_fun()")
         a_fun()
         print("I'm flower executed after a_fun()")
-    return wrapperFun()
+    return wrapperFun
 
 def fun_i_need_decoration():
     print("I'm the function that needs some beautiful flower decoration to remove my foul smell!")
 
-fun_i_need_decoration = im_decorator(fun_i_need_decoration)   # Decorator in simple terms
-# Now, fun_i_need_decoration() is wrapped inside the wrapperFun()
+fun_i_need_decoration = im_decorator(fun_i_need_decoration)
+fun_i_need_decoration()
+# That is what Decorators is in simple terms!
 
 print("------------↑ and ↓ both are same!------------------")
 
 @im_decorator
 def fun_i_need_decoration():
     print("I'm the function that needs some beautiful flower decoration to remove my foul smell!")
+    
+fun_i_need_decoration()
+
+print(fun_i_need_decoration.__name__) # prints wrapperFun...which is not what we expected...it should have printed fun_i_need_decoration
+# Well, our function was replaced by wrapperFun. It overrode the name and docstring of our function. Luckily, Python provides us a simple function to solve this problem and that is functools.wraps.
+
+print("--------------Solution------------------")
+from functools import wraps
+
+def im_decorator(a_fun):
+    @wraps(a_fun)
+    def wrapperFun():
+        print("I'm flower executed before a_fun()")
+        a_fun()
+        print("I'm flower executed after a_fun()")
+    return wrapperFun
+    
+@im_decorator
+def fun_i_need_decoration():
+    print("I'm the function that needs some beautiful flower decoration to remove my foul smell!")
+    
+fun_i_need_decoration()
+print(fun_i_need_decoration.__name__)  # prints fun_i_need_decoration
+    
