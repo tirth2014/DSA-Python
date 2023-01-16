94441d5c2b1096e65f40cb8edaa4916d0709fdcd
Tirth Patel <tirth99092@gmail.com>
2023-01-16 11:44:04 +0530

    Create DecoratorsExample.py

---
+++ basics-of-python/DecoratorsExample.py
@@ -0,0 +1,19 @@
+# DECORATORS EXAMPLE:
+
+def im_decorator(a_fun):
+    def wrapperFun():
+        print("I'm flower executed before a_fun()")
+        a_fun()
+        print("I'm flower executed after a_fun()")
+    return wrapperFun()
+
+def fun_i_need_decoration():
+    print("I'm the function that needs some beautiful flower decoration to remove my foul smell!")
+
+fun_i_need_decoration = im_decorator(fun_i_need_decoration)   # Decorator in simple terms
+
+print("------------↑ and ↓ both are same!------------------")
+
+@im_decorator
+def fun_i_need_decoration():
+    print("I'm the function that needs some beautiful flower decoration to remove my foul smell!")
