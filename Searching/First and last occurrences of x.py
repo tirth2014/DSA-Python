e0fff9a970d4e1e583938965a325a292a6fa7f27
Tirth Patel <tirth99092@gmail.com>
2023-03-12 16:15:38 +0530

    Create First and last occurrences of x.py

---
+++ Searching/First and last occurrences of x.py
@@ -0,0 +1,54 @@
+'''
+Approach: Binary Search
+Time Complexity: O(logn)
+Space Complexity: O(1)
+'''
+def firstOccurence(arr,n,x):
+    l,r = 0,n-1
+    first = -1
+    while l <= r:
+        m = l + (r-l)//2
+        if arr[m] == x:
+            first = m
+            r = m-1
+        elif x < arr[m]:
+            r = m-1
+        else:
+            l = m+1
+    return first
+        
+def lastOccurence(arr,n,x):
+    l,r = 0,n-1
+    last = -1
+    while l <= r:
+        m = l + (r-l)//2
+        if arr[m] == x:
+            last = m
+            l = m+1
+        elif x < arr[m]:
+            r = m-1
+        else:
+            l = m+1
+            
+    return last
+
+def find(arr,n,x):
+    first = firstOccurence(arr,n,x)
+    last =  lastOccurence(arr,n,x)
+    return first,last
+        
+    
+
+
+
+#{ 
+ # Driver Code Starts
+t=int(input())
+for _ in range(0,t):
+    l=list(map(int,input().split()))
+    n=l[0]
+    x=l[1]
+    arr=list(map(int,input().split()))
+    ans=find(arr,n,x)
+    print(*ans)
+# } Driver Code Ends
