ff9f57edcef179ca8a1426d83b3558227b2d31d7
Tirth Patel <tirth99092@gmail.com>
2023-03-16 07:01:51 +0530

    Update mergeSort.py

--- Sorting/mergeSort.py
+++ Sorting/mergeSort.py
@@ -6,6 +6,16 @@ A merge sort consists of several passes over the input. The first pass merges se
 the second merges segments of size 2, and the i_{th} pass merges segments of size 2^i-1. 
 Thus, the total number of passes is [log2n]. As merge showed, we can merge two sorted segments in linear time, which means that each pass takes O(n) time.
 Since there are [log2n] passes, the total computing time is O(nlogn).
+
+The merge sort algorithm requires an additional memory space of 0(n) for the temporary left and right subarrays.
+
+Advantages of Merge Sort:
+Merge sort has a time complexity of O(n log n), which means it is relatively efficient for sorting large datasets.
+Merge sort is a stable sort, which means that the order of elements with equal values is preserved during the sort.
+It is easy to implement thus making it a good choice for many applications.
+It is useful for external sorting. This is because merge sort can handle large datasets, it is often used for external sorting, where the data being sorted does not fit in memory.
+The merge sort algorithm can be easily parallelized, which means it can take advantage of multiple processors or cores to sort the data more quickly.
+Merge sort requires relatively few additional resources (such as memory) to perform the sort. This makes it a good choice for systems with limited resources.
 """
 
 def mergeSort(arr):
