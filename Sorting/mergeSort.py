dddbe948bb1af2471e4ebc52e9040146d71800d0
Tirth Patel <tirth99092@gmail.com>
2023-03-16 06:58:41 +0530

    Update mergeSort.py

--- Sorting/mergeSort.py
+++ Sorting/mergeSort.py
@@ -1,3 +1,13 @@
+"""
+Merger sort is not in-place (requires extra memory space) and it's stable (preserves order) algorithm
+
+Analysis of Merge Sort:
+A merge sort consists of several passes over the input. The first pass merges segments of size 1, 
+the second merges segments of size 2, and the i_{th} pass merges segments of size 2^i-1. 
+Thus, the total number of passes is [log2n]. As merge showed, we can merge two sorted segments in linear time, which means that each pass takes O(n) time.
+Since there are [log2n] passes, the total computing time is O(nlogn).
+"""
+
 def mergeSort(arr):
     if len(arr) > 1:
         mid = len(arr) // 2   # Floor division of length of array
@@ -7,9 +17,9 @@ def mergeSort(arr):
         # Recursively call mergeSort function on left and right subarrays respectively till array is not broken in singleton elements.
         mergeSort(L)
         mergeSort(R)
-
+       
+        # Merge Step:
         i, j, k = 0, 0, 0
-
         while i < len(L) and j < len(R):
             if L[i] < R[j]:
                 arr[k] = L[i]
@@ -18,6 +28,8 @@ def mergeSort(arr):
                 arr[k] = R[j]
                 j += 1
             k += 1
+        
+        # Checking if any element was left
         while i < len(L):
             arr[k] = L[i]
             i += 1
