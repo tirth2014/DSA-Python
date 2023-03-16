fdda9b312df7f000dddf844b5d2b3e8da88679d3
Tirth Patel <tirth99092@gmail.com>
2023-03-16 07:13:40 +0530

    Update Count Inversions in Array.py

--- Arrays Part-II/Count Inversions in Array.py
+++ Arrays Part-II/Count Inversions in Array.py
@@ -1,3 +1,8 @@
+# Inversion Count:
+# For an array, inversion count indicates how far (or close) the array is from being sorted.
+# If array is already sorted then the inversion count is 0. If an array is sorted in the reverse order then the inversion count is the maximum. 
+# Formally, two elements a[i] and a[j] form an inversion if a[i] > a[j] and i < j.
+
 # Solution - 1 (Using Merge Sort):
 
 class Solution:
