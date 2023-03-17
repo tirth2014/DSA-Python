a71d8f98bb9b91d61989225c48157665aaaa3623
Tirth Patel <tirth99092@gmail.com>
2023-03-17 06:46:32 +0530

    Update 414. Third Maximum Number.py

--- Sorting/414. Third Maximum Number.py
+++ Sorting/414. Third Maximum Number.py
@@ -1,5 +1,6 @@
 # My merge sort approach 
-# bau kharab time complexity aave chhe - beats only 6% and memory beats only 16%
+# Time complexity: O(NlogN)
+# Space complexity: O(N)
 
 class Solution:
     def merge(self, arr, L, R):
@@ -43,10 +44,37 @@ class Solution:
         nums = self.mergeSort(nums, len(nums))  # sorted nums
         seen = set()
         seen = [x for x in nums if not (x in seen or seen.add(x))]
-        third_max = seen[-3] if len(seen) >= 3 else False
+        third_max = seen[-3] if len(seen) >= 3 else float('-inf')
         first_max = seen[-1] if seen else False
-        return third_max if (third_max or str(third_max)=='0') else first_max
+        return third_max if third_max != float('-inf') else first_max
+#         False ni jagya e float('-inf') which is equal to -2**32 i.e., max. negative number
+#         third_max = seen[-3] if len(seen) >= 3 else False
+#         first_max = seen[-1] if seen else False
+#         return third_max if (third_max or str(third_max)=='0') else first_max'
       
       
 # Follow up: Can you find an O(n) solution?
-      
+
+# Time Complexity :- O(N)
+# Space complexity: O(1)
+class Solution:
+    def thirdMax(self, nums: List[int]) -> int:
+        nums = list(set(nums))
+        maxs = [float('-inf')]*3
+        for num in nums:
+            if num > maxs[0]:
+                maxs = [num,maxs[0],maxs[1]]
+            elif num > maxs[1]:
+                maxs = [maxs[0],num,maxs[1]]
+            elif num > maxs[2]:
+                maxs = [maxs[0],maxs[1],num]
+
+        return maxs[2] if maxs[2] != float('-inf') else maxs[0]
+    
+    
+# Time Complexity :- O(NlogN)
+class Solution:
+    def thirdMax(self, nums: List[int]) -> int:
+        nums = [*set(nums)]
+        nums.sort()
+        return nums[-1] if len(nums) < 3 else nums[-3]      
