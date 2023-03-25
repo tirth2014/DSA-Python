d237aeab9d7dc28d6a823002b8853cc44ecc1dbd
Tirth Patel <tirth99092@gmail.com>
2023-03-25 19:01:22 +0530

    Create 410. Split Array Largest Sum.py

---
+++ Searching/410. Split Array Largest Sum.py
@@ -0,0 +1,29 @@
+# The problem is just like LC 1011
+# We can use our Binary Search template for this.
+class Solution:
+    def splitArray(self, nums: List[int], k: int) -> int:   
+        
+        def feasible(threshold):  # Condition function...returns True if all subarrays sum is < threshold else False
+            cnt = 1
+            total = 0
+            for num in nums:
+                total += num
+                if total > threshold:  # Total sum of current subarray is greater than threshold...so we must put this num in next subarray
+                    total = num
+                    cnt += 1   # Counts the length of current subarray
+                    if cnt > k: return False # Number of subarrays exceeds k
+            return True 
+        
+        left,right = max(nums),sum(nums)   # Maximum sum of any subarray will always lie between these two ranges
+        while left < right:
+            mid = left + (right-left)//2
+            if feasible(mid):  
+                right = mid   # Feasible chhe to aapne haji nani threshold value mate check kariye
+            else:
+                left = mid + 1
+        return left
+
+
+# Example:   nums = [10,7,2,12,5,11,7,13], k = 4
+#            left = 13,  right = 67 
+#            ans = 19      
