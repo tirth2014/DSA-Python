43c56b9a6bfc26f47da58e10d2d6ce3aa33481d8
Tirth Patel <tirth99092@gmail.com>
2023-04-10 07:33:18 +0530

    Update PRINT COUNT PATTERN.py

--- Recursion & Backtracking/PRINT COUNT PATTERN.py
+++ Recursion & Backtracking/PRINT COUNT PATTERN.py
@@ -4,10 +4,19 @@ def recursive_fun():
   condition satisfied => return 1
   condition not satisfied => return 0
   
+  # If there are 2 recursive calls
   l = fun()
   r = fun()
   
   return l+r
+
+
+  # else if there are "n" recursive calls [ This pattern is used in N-Queens problem]
+  s = 0
+  recursive_fun(i = 1 -> n)
+    s += recursive_fun()
+  return s
+  
 
 # This pattern is also imp. in dynamic programming
   
