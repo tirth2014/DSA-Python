2ae47696ea9897bad63046fef55aa65032a11272
Tirth Patel <tirth99092@gmail.com>
2023-04-10 07:29:51 +0530

    Create PRINT COUNT PATTERN.py

---
+++ Recursion & Backtracking/PRINT COUNT PATTERN.py
@@ -0,0 +1,13 @@
+def recursive_fun():
+  
+  BASE CASE
+  condition satisfied => return 1
+  condition not satisfied => return 0
+  
+  l = fun()
+  r = fun()
+  
+  return l+r
+
+# This pattern is also imp. in dynamic programming
+  
