0f83857b80d194f4fb2db605c27ecd050a6c1a0a
Tirth Patel <tirth99092@gmail.com>
2023-06-28 08:53:35 +0530

    Create solution.py

---
+++ Recursion & Backtracking/77. Combinations/solution.py
@@ -0,0 +1,37 @@
+#1 
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        ans = []
+        def backtrack(remain,arr,nxt):
+            # solution found
+            if remain == 0:
+                ans.append(arr[:])
+            else:
+                # iterate through all candidates
+                for i in range(nxt,n+1):
+                    # add candidate
+                    arr.append(i)
+                    # backtrack
+                    backtrack(remain-1,arr,i+1)
+                    # remove candidate
+                    arr.pop()
+        backtrack(k,[],1)
+        return ans
+
+#2
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        combs = [[]]
+        for _ in range(k):
+            temp = []
+            for c in combs:
+                for i in range(1, c[0] if c else n+1):
+                    temp.append([i]+c)
+            combs = temp
+        return combs
+
+#3 (fastest)
+from itertools import combinations 
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        return list(combinations(range(1,n+1),k))
