049482b04af43fea41663b5e3b36a934c03060d5
Tirth Patel <tirth99092@gmail.com>
2023-07-25 06:44:38 +0530

    Create Solution.py

---
+++ Recursion & Backtracking/216. Combination Sum III/Solution.py
@@ -0,0 +1,48 @@
+# Beats 8.44%
+class Solution:
+    def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+        ans = []
+
+        def helper(i=1,arr=[]):
+            if len(arr) == k or i > 9:
+                if sum(arr) == n and len(arr) == k:
+                    ans.append(arr[:])
+                return
+
+            #pick
+            arr.append(i)
+            helper(i+1)
+            arr.pop()
+
+            #not pick
+            helper(i+1)
+
+        helper()
+        return ans
+
+
+# Slightly optimized:
+# Beats 79.64%
+class Solution:
+    def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+        ans = []
+
+        def helper(i=1,arr=[],curr_sum=0):
+            if len(arr) == k:
+                if sum(arr) == n:
+                    ans.append(arr[:])
+                return
+
+            if i > 9 or curr_sum > n:
+                return
+
+            #pick
+            arr.append(i)
+            helper(i+1, arr, curr_sum+i)
+            arr.pop()
+
+            #not pick
+            helper(i+1, arr, curr_sum)
+
+        helper()
+        return ans
