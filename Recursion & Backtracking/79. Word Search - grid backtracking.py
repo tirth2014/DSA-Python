b6ef7c6bb1a0b8c0213fad2fa22c7747ebe0350f
Tirth Patel <tirth99092@gmail.com>
2023-08-01 07:22:28 +0530

    Update and rename 79. Word Search.py to 79. Word Search - grid backtracking.py

--- Recursion & Backtracking/79. Word Search - grid backtracking.py
+++ Recursion & Backtracking/79. Word Search - grid backtracking.py
@@ -1,7 +1,16 @@
-'''
+"""
 Given an m x n grid of characters board and a string word, return true if word exists in the grid.
 The word can be constructed from letters of sequentially adjacent cells, where adjacent cells are horizontally or vertically neighboring. The same letter cell may not be used more than once.
-'''
+https://leetcode.com/problems/word-search/
+
+Example:
+Input: 
+board = [["A","B","C","E"],
+        ["S","F","C","S"],
+        ["A","D","E","E"]], 
+word = "ABCCED"
+Output: true
+"""
 
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
