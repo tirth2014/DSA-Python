898bc1bc9500f0ef962c523dc8973860009f5bc9
Tirth Patel <tirth99092@gmail.com>
2023-09-10 22:07:14 +0530

    Update print_bin_val_of_first_n_nos.py

--- Bit-Manipulation/print_bin_val_of_first_n_nos.py
+++ Bit-Manipulation/print_bin_val_of_first_n_nos.py
@@ -14,9 +14,8 @@ if __name__ == '__main__':
         print()
 
 
-# Using bitwise operators
+# Using bitwise operators (recursively)
 if __name__ == '__main__':
-
     def print_binary(num):
         if num > 1:
             # Right shift by 1 position means divide by 2
@@ -24,7 +23,6 @@ if __name__ == '__main__':
             print_binary(num >> 1)
         # odd => 1 and even => 0
         print(num & 1, end=" ")
-
 
     N = int(input("enter N: "))
     for i in range(1,N+1):
@@ -39,3 +37,17 @@ if __name__ == '__main__':
 # 3: 1 1 
 # 4: 1 0 0 
 # 5: 1 0 1
+
+
+# Iterative
+def print_binary(num):
+    bin_n = ''
+    while num:
+        bin_n = str(num & 1) + bin_n
+        num = num >> 1
+    return bin_n
+
+N = int(input("enter N: "))
+for i in range(1,N+1):
+    print(i, end=': ')
+    print(print_binary(i))
