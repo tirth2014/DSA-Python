ecd9947836ce01597ecb486109524f7c3b64ee7a
Tirth Patel <tirth99092@gmail.com>
2023-09-10 12:08:03 +0530

    Create Count total set bits.py

---
+++ Bit-Manipulation/Count total set bits.py
@@ -0,0 +1,21 @@
+if __name__ == '__main__':
+
+    def set_bits_in_binary(num,res):
+        if num > 1:
+            res = set_bits_in_binary(num >> 1,res)
+        if num & 1:
+            res += 1
+        return res
+
+    def countSetBits(N: int) -> int:
+        res = 0
+        for num in range(1, N + 1):
+            print(num, end=': ')
+            print(set_bits_in_binary(num,res), end=', ')
+
+    N = int(input("N: "))
+    countSetBits(N)
+
+# Output
+# N: 16
+# 1: 1, 2: 1, 3: 2, 4: 1, 5: 2, 6: 2, 7: 3, 8: 1, 9: 2, 10: 2, 11: 3, 12: 2, 13: 3, 14: 3, 15: 4, 16: 1,
