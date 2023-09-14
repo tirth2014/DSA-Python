2e22cedd2045e42ff0edf49a892cf7a550fd42c6
Tirth Patel <tirth99092@gmail.com>
2023-09-15 04:46:54 +0530

    Update prime_factors_of_num.py

--- Math/prime_factors_of_num.py
+++ Math/prime_factors_of_num.py
@@ -14,3 +14,20 @@ if __name__ == '__main__':
 # output console:
 # n: 48
 # 2 2 2 2 3 
+
+
+# A little optimized:
+# T.C: O(sqrt(n)) worst case
+if __name__ == '__main__':
+    # Every no. has prime factorization apart from no. itself that's <= sqrt(n)
+    # Using this property to optimize
+    def prime_factorization(n):
+        for i in range(2,int(n**0.5)+1):
+            while n % i == 0:
+                print(i,end=' ')
+                n /= i
+        if n > 1:
+            print(int(n), end=' ')
+
+    n = int(input("n: "))
+    ans = prime_factorization(n)
