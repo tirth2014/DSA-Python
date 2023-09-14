064b6fe1ef4ac49e5b93b9c55a9d6d2cadb3032b
Tirth Patel <tirth99092@gmail.com>
2023-09-15 05:21:55 +0530

    Update prime_factors_of_num.py

--- Math/prime_factors_of_num.py
+++ Math/prime_factors_of_num.py
@@ -31,3 +31,29 @@ if __name__ == '__main__':
 
     n = int(input("n: "))
     prime_factorization(n)
+
+
+# A maximum no. of prime factors a number can have = log2(n)...as 2 is the smallest prime factor for any number.
+# Using this property and Sieve of Eratosthenes...we'll maintain a sieve or spf array (smallest prime factor array)
+# All the numbers in the spf array will have their smallest prime factor...and for prime numbers the number itself.
+# It's the most efficient method to find prime factors of any given number for even larger testcases
+# Worst Case T.C:  O(log2(n)) i.e. log(n) base 2 
+
+if __name__ == '__main__':
+    def prime_factorization(n):
+        spf = [i for i in range(n+1)]
+        for el in range(2, int(n**0.5) + 1):
+            i = 2
+            while el*i <= n:
+                if spf[el*i] == el*i:
+                    spf[el*i] = el
+                i += 1
+        # Our sieve (spf array) is ready to use now with O(1) lookup.
+        num = n
+        while num > 1:
+            print(spf[num],end=' ')
+            num //= spf[num]
+
+
+    n = int(input("n: "))
+    prime_factorization(n)
