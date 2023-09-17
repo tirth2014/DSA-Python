a7758816fc0220b01fb1b6c8a8e6d997111ab359
Tirth Patel <tirth99092@gmail.com>
2023-09-17 23:04:08 +0530

    Create 496. Next Greater Element I.py

---
+++ Stacks_&_Queues/496. Next Greater Element I.py
@@ -0,0 +1,93 @@
+"""
+496. Next Greater Element I
+
+The next greater element of some element x in an array is the first greater element that is to the right of x in the same array.
+You are given two distinct 0-indexed integer arrays nums1 and nums2, where nums1 is a subset of nums2.
+For each 0 <= i < nums1.length, find the index j such that nums1[i] == nums2[j] and determine the next greater element of nums2[j] in nums2.
+If there is no next greater element, then the answer for this query is -1.
+Return an array ans of length nums1.length such that ans[i] is the next greater element as described above.
+
+Example 1:
+Input: nums1 = [4,1,2], nums2 = [1,3,4,2]
+Output: [-1,3,-1]
+Explanation: The next greater element for each value of nums1 is as follows:
+- 4 is underlined in nums2 = [1,3,4,2]. There is no next greater element, so the answer is -1.
+- 1 is underlined in nums2 = [1,3,4,2]. The next greater element is 3.
+- 2 is underlined in nums2 = [1,3,4,2]. There is no next greater element, so the answer is -1.
+
+Example 2:
+Input: nums1 = [2,4], nums2 = [1,2,3,4]
+Output: [3,-1]
+Explanation: The next greater element for each value of nums1 is as follows:
+- 2 is underlined in nums2 = [1,2,3,4]. The next greater element is 3.
+- 4 is underlined in nums2 = [1,2,3,4]. There is no next greater element, so the answer is -1.
+ 
+Constraints:
+1 <= nums1.length <= nums2.length <= 1000
+0 <= nums1[i], nums2[i] <= 10^4
+All integers in nums1 and nums2 are unique.
+All the integers of nums1 also appear in nums2.
+ 
+Follow up: Could you find an O(nums1.length + nums2.length) solution?
+"""
+
+# Naive Solution - O(N^2)
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        res = [-1]*len(nums1)
+        for i1, ele in enumerate(nums1):
+            for ind, ele2 in enumerate(nums2):
+                if ele2 == ele:
+                    for i in range(ind+1, len(nums2)):
+                        if nums2[i] > ele2:
+                            res[i1] = nums2[i]
+                            break
+                    break
+        return res
+
+
+
+# Optimal - Using stack
+# => Intuition:
+#     Basically the problem says, if in nums1 we are working on 4, then in nums2 first find where is 4 
+#     and from that index find the next number greater than 4 in nums2. 
+#     We can see that the solution is always coming from the reverse side of the list nums2. 
+#     This should tell us to use stack.
+
+import ast
+from typing import List
+
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        res = [-1]*len(nums1)
+        stack = []
+        next_gr_dct = {}
+
+        # O(len(nums2))
+        for ele in nums2[::-1]:
+            # Keep popping small elements from stack till stack top is > ele or stack becomes empty
+            while stack and stack[-1] <= ele:
+                stack.pop()
+            if not stack:
+                # No greater ele. on right side
+                next_gr_dct[ele] = -1
+            elif stack[-1] > ele:
+                # stack top ele. is the next greater element
+                next_gr_dct[ele] = stack[-1]
+            # Append current ele. to stack in any case.
+            stack.append(ele)
+
+        # O(len(nums1))
+        for i, ele in enumerate(nums1):
+            res[i] = next_gr_dct.get(ele, -1)
+
+        return res
+
+if __name__ == '__main__':
+    ob = Solution()
+    # arr = list(map(int, input("arr: ").split()))
+    arr1 = ast.literal_eval(input("arr1: "))
+    arr2 = ast.literal_eval(input("arr2: "))
+    ans = ob.nextGreaterElement(arr1,arr2)
+
+    print(ans)
