67437650e86dda128146cbc53b9099509306e911
Tirth Patel <tirth99092@gmail.com>
2023-09-19 06:00:24 +0530

    Update contiguous-subarrays.py

--- Recursion & Backtracking/contiguous-subarrays.py
+++ Recursion & Backtracking/contiguous-subarrays.py
@@ -1,3 +1,4 @@
+# Only works for array with distinct (unique) elements
 class Solution:
     def sumSubarrayMins(self, arr: List[int]) -> List[int]:
         subarrays = []
@@ -8,7 +9,7 @@ class Solution:
                 return
 
             # pick
-            if not curr or curr[-1] == arr[i-1]:  # this condition prevents non-contiguous elements from being added.
+            if not curr or curr[-1] == arr[i-1]:  # this condition prevents non-contiguous distinct elements from being added.
                 curr.append(arr[i])
                 dfs(curr, i+1)
                 curr.pop()
