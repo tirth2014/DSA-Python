8b8eaca02258bcdc7f5cb49bc77bee1668580ead
Tirth Patel <tirth99092@gmail.com>
2023-12-17 20:02:27 +0530

    Create 930. Binary Subarrays With Sum.py

---
+++ Sliding Window & Two Pointer/930. Binary Subarrays With Sum.py
@@ -0,0 +1,37 @@
+
+import ast
+from typing import List
+import heapq
+
+# Space O(N)
+# Time O(N)
+class Solution:
+    def numSubarraysWithSum(self, nums: List[int], k: int) -> int:
+        # Using prefix sum algorithm
+        # This approach works for array with all elements either +ve, -ve etc...
+        # at every index if prefix sum is 's', we're looking for
+        # no. of subarrays before that element with prefix_sum 's-k'
+        # storing 0 in dict/hashmap(pref_sum: cnt) is very important
+        # s = prefix sum
+        # k = goal
+        # dict = prefix sum count dictionary
+        dict = {}
+        s, res = 0, 0
+        dict[0] = 1
+        for i in range(len(nums)):
+            s += nums[i]
+            res += dict.get(s - k, 0)
+            dict[s] = dict.get(s, 0) + 1
+        return res
+      
+
+if __name__ == '__main__':
+    ob = Solution()
+    for t in range(int(input("#testcases: "))):
+        # arr = list(map(int, input("arr: ").split()))
+        arr = ast.literal_eval(input("arr: "))
+        # st = input("string: ")
+        # num = input("num string: ")
+        k = int(input("goal: "))
+        ans = ob.numSubarraysWithSum(arr, k)
+        print(ans)
